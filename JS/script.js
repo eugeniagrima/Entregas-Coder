0046468e66a6bc937d7a9cdb5b02e75df815db8b
Eugenia Grima <eugeniagrima@gmail.com>
2022-06-03 18:45:55 -0300

    agregue javascript

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+alert ("Bienvenida!")
+let nombreUsuario = prompt ("Ingresá tu nombre completo")
+let edadUsuario = prompt ("Ingresá tu edad")
+alert ("Tu nombre es" + " " + nombreUsuario + " " + "y tenes" + " " + edadUsuario + " " + "años");
+if (nombreUsuario == "") {
+    alert ("No ingresaste tu nombre!")
+}
+else {
+    alert ("Tu nombre es" + "" + nombreUsuario);
+}
