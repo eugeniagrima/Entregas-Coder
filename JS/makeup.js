c4896cd7da171ca11b4da27daa6cccd2f952f384
Eugenia Grima <eugeniagrima@gmail.com>
2022-06-14 17:43:31 -0300

    agregue arrays

---
+++ JS/makeup.js
@@ -0,0 +1,6 @@
+const tiposMakeUp = ["Clases de automaquillaje", "Maquillaje social", "Maquillaje para novia", "Maquillaje artístico"];
+const otrosMakeUp = ["Maquillaje teatral", "Maquillaje para fotos", "Otros maquillajes"];
+tiposMakeUp.forEach (make => {
+    alert ("Tipos de Make-up: ${make}");
+}
+    )
---
+++ JS/tfaciales.js
@@ -0,0 +1,13 @@
+function Tratamiento (nombre, numero) {
+    this.nombre = nombre;
+    this.numero = numero;
+}
+const tratamiento1 = new Tratamiento ("Higiene Base", 1);
+const tratamiento2 = new Tratamiento ("Higiene Plus", 2);
+const tratamiento3 = new Tratamiento ("Citrus Essence", 3);
+const tratamiento4 = new Tratamiento ("Infusión Hialurónico", 4);
+
+console.log (tratamiento1);
+console.log (tratamiento2);
+console.log (tratamiento3);
+console.log (tratamiento4);
