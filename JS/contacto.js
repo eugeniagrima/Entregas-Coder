5dc2e579a0a06f67d329d6642f8c6081029fdf7d
Eugenia Grima <eugeniagrima@gmail.com>
2022-06-14 12:28:03 -0300

    agregue funcion a contacto

--- JS/contacto.js
+++ JS/contacto.js
@@ -19,6 +19,6 @@ function CuantasVecesTeAtendiste() {
     let operador = prompt("+, -, * o /?")
     let num1 = Number (prompt("Ingresá el primer número"));
     let num2 = Number (prompt("Ingresá el segundo número"));
-   alert ("Pensás que tenemos ${calculadora (num1, num2, operador)} consultas diarias");
+   alert ("Pensás que tenemos" + " "+ calculadora(num1, num2, operador) + " " + "consultas diarias")
 }
 CuantasVecesTeAtendiste()
