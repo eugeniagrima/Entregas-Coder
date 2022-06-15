f5abdd559844c02efc34df583fb73b23b2904322
Eugenia Grima <eugeniagrima@gmail.com>
2022-06-15 12:41:19 -0300

    make up

--- JS/makeup.js
+++ JS/makeup.js
@@ -1,6 +1,15 @@
 const tiposMakeUp = ["Clases de automaquillaje", "Maquillaje social", "Maquillaje para novia", "Maquillaje artístico"];
 const otrosMakeUp = ["Maquillaje teatral", "Maquillaje para fotos", "Otros maquillajes"];
+for (const tiposMak of tiposMakeUp) {
+    console.log (tiposMak);
+}
+for (const tiposMak of tiposMakeUp) {
+    alert ("Tipos de Make-up: ${tiposMak}"); //SI LE PONGO "" QUEDA OK SIN ERRORES PERO NO SE IMPRIME NADA EN LA WEB
+}
+for (const tiposMak of tiposMakeUp) {
+    alert ("Tipos de Make-up:" ${tiposMak}); //SI CIERRO LAS "" ACA ME SALTA ERROR PERO SE VE BIEN, DE TODOS MODOS EN LA WEB NO SE VE COMO QUIERO.
+}
 tiposMakeUp.forEach (make => {
-    alert ("Tipos de Make-up: ${make}");
+    alert ("Tipos de Make-up: + $(make)");
 }
     )
