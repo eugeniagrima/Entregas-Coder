07a130ec92b4cd196ead7a9ac5483177e38fbdfb
Eugenia Grima <eugeniagrima@gmail.com>
2022-06-23 12:13:27 -0300

    dom

--- JS/dom.js
+++ JS/dom.js
@@ -1,13 +1,13 @@
 const preguntasFrecuentes = ["Quiero recibir mas info", "Quiero precios de maquillajes", "En qué zona están?", "Necesito que me contacten para darme un turno"];
 const comentarios = document.getElementById("Multi");
 
-for (comentario of preguntasFrecuentes) {
-    const li = document.createElement("li");
-    li.innerHTML = comentario;
-    console.log(li);
-    comentarios.append(li);
+for (let comentario of preguntasFrecuentes) {
+    const lista = document.createElement("li");
+    lista.innerText = comentario;
+    console.log(lista);
+    comentarios.append(lista);
 }
 
-const inputComent = document.querySelector ("#Multi");
+const inputComent = document.querySelector ("#areatexto");
 inputComent.value = "Dejanos aca tus comentarios!"; 
 console.log (inputComent.value);
