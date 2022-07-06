91cf182712d78fe70369662cd20b62644c44dd99
Eugenia Grima <eugeniagrima@gmail.com>
2022-07-05 21:14:46 -0300

    librerias

--- JS/contacto.js
+++ JS/contacto.js
@@ -22,4 +22,30 @@ function CuantasVecesTeAtendiste() {
    document.write ("Pensás que tenemos " + calculadora(num1, num2, operador) + " consultas diarias")
 }
 CuantasVecesTeAtendiste()
-//QUE TENDRIA QUE HACER ACA PARA SACAR EL ALERT? 
+//QUE TENDRIA QUE HACER ACA PARA SACAR EL ALERT?
+//NO ME MUESTRA ESTO EN LA WEB. 
+const { value: text } = await Swal.fire({
+    input: 'textarea',
+    inputLabel: 'Ya te atendiste con nosotras?',
+    inputPlaceholder: 'Escribí tu respuesta aca!',
+    inputAttributes: {
+      'aria-label': 'Escribí tu respuesta aca!'
+    },
+    showCancelButton: true
+  })
+  
+  if (text) {
+    Swal.fire(text)
+  }
+  Swal.fire({
+    title: 'Cuántos años tenés?',
+    icon: 'question',
+    input: 'range',
+    inputLabel: 'Tu edad',
+    inputAttributes: {
+      min: 16,
+      max: 100,
+      step: 1
+    },
+    inputValue: 25
+  })
